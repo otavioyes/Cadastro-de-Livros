4b539e9bc77d6759649648df2157ab591f4f9146
Otavio Correa <correaotavio0409@gmail.com>
2024-07-05 07:19:44 -0300

    Cadastrando Livros Usando Array

--- CadastroLivroStruct/CadastroLivroStruct.cpp
+++ CadastroLivroStruct/CadastroLivroStruct.cpp
@@ -1,42 +1,54 @@
 #include <iostream>
 #include <string>
+#include <locale>
 
 
 struct Livro {
 
 	int ID;
 	int AnoDeLancamento;
-	std::string Tituto;
+	std::string Titulo;
 	std::string Autor;
 	std::string Editora;
-	
 };
-
 int main() {
-
-	struct Livro livro;
-
-	std::cout << "*****Cadastro de Livro*****\n";
 	
-	std::cout << "Digite o ID do livro: ";
-	std::cin >> livro.ID;
+	setlocale(LC_ALL, "portuguese");
 
-	std::cout << "\nDigite o Ano de Lançamento do Livro: ";
-	std::cin >> livro.AnoDeLancamento;
+	Livro livro[5];
 
-	std::cout << "\nDigite o Titulo do livro: ";
-	std::cin.ignore();
-	std::getline(std::cin, livro.Tituto);
+	for (int i = 0; i < 5; i++) {
 
-	std::cout << "\nDigite o Autor do livro: ";
-	std::getline(std::cin, livro.Autor);
+		std::cout << "*****Cadastro de Livro*****\n";
 
-	std::cout << "\nDigite a Editora do livro: ";
-	std::getline(std::cin, livro.Editora);
+		std::cout << "Digite o ID do livro " << i + 1 << " : ";
+		std::cin >> livro[i].ID;
 
+		std::cout << "\nDigite o Ano de Lançamento do Livro " << i + 1 << " : ";
+		std::cin >> livro[i].AnoDeLancamento;
 
-	std::cout << "\nID: " << livro.ID << "\nAno de lançamento: " << livro.AnoDeLancamento << "\nTitulo : " << livro.Tituto << "\nAutor : " << livro.Autor << "\nAutor : " << livro.Editora << '\n';
+		std::cout << "\nDigite o Titulo do livro " << i + 1 << " : ";
+		std::cin.ignore();
+		std::getline(std::cin, livro[i].Titulo);
 
+		std::cout << "\nDigite o Autor do livro " << i + 1 << " : ";
+		std::getline(std::cin, livro[i].Autor);
+
+		std::cout << "\nDigite a Editora do livro " << i + 1 << " : ";
+		std::getline(std::cin, livro[i].Editora);
+		system("CLS");
+
+	}
+	std::cout << "**********Livros cadastrados**********\n";
+	for (int i = 0; i < 5; i++) {
+
+		std::cout << "\nID: " << livro[i].ID << "\nAno de lançamento: " << livro[i].AnoDeLancamento << "\nTitulo : " << livro[i].Titulo << "\nAutor : " << livro[i].Autor << "\nAutor : " << livro[i].Editora << '\n';
+
+		std::cout << "----------------------------------------------------------------------" << '\n';
+	}
+	std::cout << "*****************************************************************" << '\n';
+
+	
 
 	return 0;
 }
